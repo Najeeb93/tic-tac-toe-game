fc298f1184664fe1ae81a87c330ca986b1d41da5
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-05-05 19:53:00 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var boxes = document.getElementById("Boxes");
+
+for(let index = 0; index < 9;  index++) {
+    boxes.innerHTML += `<div onclick={tick(this)} class='box'></div>`
+}
+
+var isFirstUser = true;
+
+function tick(box) {
+    if(isFirstUser === true){
+        box.innerHTML = `<img src="./cross.png" />`;
+        isFirstUser = false;
+    }else {
+        box.innerHTML = `<img src="./tick.png" />`;
+        isFirstUser = true;
+}
+}
+function clearBoxes() {
+    let boxes = document.getElementsByClassName("box")
+    for (i = 0; i < boxes.length; i++) {
+      boxes[i].innerHTML = ""
+    }
+  }
